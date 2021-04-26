d7d9a591c91e6ed3a48fe2776f0a9a48d89192c1
ThickFive <thickfive@live.com>
2021-04-26 14:26:20 +0800

    count lines of code

---
+++ lines.js
@@ -0,0 +1,8 @@
+const process = require('child_process');
+const username = "ThickFive";
+const since = "2021-4-1";
+const until = "2021-12-31";
+var cmd = `git log --author="${username}" --pretty=tformat: --since ==${since} --until=${until} --numstat | awk '{ add += $1; subs += $2; loc += $1 - $2 } END { printf "added lines: %s, removed lines: %s,total lines: %s", add, subs, loc }'`
+process.exec(cmd, function(error, stdout, stderr) {
+    console.log(stdout);
+});
